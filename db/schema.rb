68e6c2d71117ecc403391973991e17ca9c93934e
Tu Hoang <rebyn@me.com>
2013-08-22 15:37:52 +0700

    generate scaffold Project title:string description:text

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130822081607) do
+ActiveRecord::Schema.define(version: 20130822082847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+  enable_extension "uuid-ossp"
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end

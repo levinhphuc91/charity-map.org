09b8c72422b4c91fbd3fe902b54eb1522aa02a27
Tu Hoang <rebyn@me.com>
2013-09-02 12:11:19 +0700

    add and config locale, :vi as default

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe PagesController do
 
   describe "GET 'home'" do
+
+    it "has :vi as default locale" do
+      get 'home'
+      I18n.locale.should eq(:vi)
+    end
+
     it "returns http success" do
       get 'home'
       response.should be_success

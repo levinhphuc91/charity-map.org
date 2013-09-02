c785966d60f15c24d0090048451074ae058af39e
Tu Hoang <rebyn@me.com>
2013-09-03 00:51:32 +0700

    style Donation bar on /dashboard - WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def human_time(time)
+    time.strftime("%d/%m/%y %H:%M")
+  end
+
+  def human_currency(amount)
+    number_to_currency amount, delimiter: ".", precision: 0, unit: "đ"
+  end
 end

506609a55f338622d363e573e38f66d5f156af0f
Tu Hoang <rebyn@me.com>
2013-09-02 10:49:45 +0700

    fix rspec tests

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -25,7 +25,9 @@ describe ProjectsController do
   # This should return the minimal set of attributes required to create a valid
   # Project. As you add validations to Project, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:valid_attributes) { { "title" => "MyString", "description" => "My description",
+    "start_date" => "2013-05-23", "end_date" => "2013-05-30", "funding_goal" => 3000000,
+    "user_id" => 1, "location" => "Ho Chi Minh" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in

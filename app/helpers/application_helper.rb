2654d75d6881fb39ef0b6b9ce9625da213ed4f62
Tu Hoang <rebyn@me.com>
2013-09-03 12:41:05 +0700

    add human_status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,19 @@ module ApplicationHelper
   def human_currency(amount)
     number_to_currency amount, delimiter: ".", precision: 0, unit: "đ"
   end
+
+  def human_status(status)
+    case status
+    when "DRAFT"
+      return "Bản Nháp"
+    when "PENDING"
+      return "Đã Gửi Lên Hệ Thống"
+    when "REVIEWED"
+      return "Được Duyệt Để Gây Quỹ"
+    when "FINISHED"
+      return "Hoàn Thành"
+    else
+      return status
+    end
+  end
 end

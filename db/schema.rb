68e17e49d308d4340cf201b95d59a9f5272cc3d8
Tu Hoang <rebyn@me.com>
2013-09-03 22:56:34 +0700

    proper slug config (schema update)

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130903100907) do
+ActiveRecord::Schema.define(version: 20130903155007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-  enable_extension "uuid-ossp"
 
   create_table "configurations", force: true do |t|
     t.string   "name"
@@ -41,6 +40,17 @@ ActiveRecord::Schema.define(version: 20130903100907) do
   add_index "donations", ["project_id"], name: "index_donations_on_project_id", using: :btree
   add_index "donations", ["project_reward_id"], name: "index_donations_on_project_reward_id", using: :btree
   add_index "donations", ["user_id"], name: "index_donations_on_user_id", using: :btree
+
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 40
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "project_comments", force: true do |t|
     t.text     "content"
@@ -87,6 +97,7 @@ ActiveRecord::Schema.define(version: 20130903100907) do
     t.string   "slug"
   end
 
+  add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true, using: :btree
   add_index "projects", ["user_id"], name: "index_projects_on_user_id", using: :btree
 
   create_table "users", force: true do |t|

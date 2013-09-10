0c0ea5237f550dc88332796603099fffbef6bfe8
Tu Hoang <rebyn@me.com>
2013-09-10 18:08:35 +0700

    add default url to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ CharityMap::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'beta.charity-map.org' }
 end

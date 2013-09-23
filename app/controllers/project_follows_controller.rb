c193a5c56553759fd248e1b9bf8dc35dbb74bf56
Tu Hoang <rebyn@me.com>
2013-09-23 17:35:28 +0700

    restyle Project#follow

--- app/controllers/project_follows_controller.rb
+++ app/controllers/project_follows_controller.rb
@@ -8,11 +8,10 @@ class ProjectFollowsController < InheritedResources::Base
     if @project_follow.save
       respond_to do |format|
         format.js
-        format.html { redirect_to project_path(@project), notice: "Theo đuôi dự án thành công." }
+        format.html { redirect_to project_path(@project), notice: "Bắt đầu theo dõi dự án này." }
       end
     else
       format.html { redirect_to project_path(@project), alert: "Không thành công. Vui lòng thử lại." }
     end
   end
-
 end

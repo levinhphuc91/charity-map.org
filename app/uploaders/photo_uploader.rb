f9af0f5904c1da9f33e85a3c3d7bf03886f0e21f
Tu Hoang <rebyn@me.com>
2013-09-26 12:11:20 +0700

    misc style fixes

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -37,7 +37,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :banner do
-    process :resize_to_fill => [960, 300]
+    process :resize_to_fill => [960, 305]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.

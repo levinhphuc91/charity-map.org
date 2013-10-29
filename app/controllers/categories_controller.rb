511e26bf7d440261d5b6e68bfb1752e66ae0a0bc
Tu Hoang <rebyn@me.com>
2013-10-29 19:58:16 +0700

    only allow :index and :show for CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,17 @@
 class CategoriesController < InheritedResources::Base
+  actions :all, :except => [ :new, :create, :edit, :update, :destroy ]
   before_filter :allow_staff_only, except: :index
+
+  def index
+    index!
+    @projects = Project.public_view
+  end
+
+  def show
+    show!
+    @projects = @category.projects
+  end
+
   private
     def allow_staff_only
       redirect_to action: :index unless current_user && current_user.staff

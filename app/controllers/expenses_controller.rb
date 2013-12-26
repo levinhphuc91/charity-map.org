7fba41c23e28a2f783abf52e667ea4538471f687
Tu Hoang <rebyn@me.com>
2013-12-26 16:10:16 +0700

    add http authentication for Grants & ExpensesController

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,3 @@
 class ExpensesController < InheritedResources::Base
+  http_basic_authenticate_with name: ENV["HTTP_USERNAME"], password: ENV["HTTP_PASSWORD"]
 end
--- app/controllers/grants_controller.rb
+++ app/controllers/grants_controller.rb
@@ -1,4 +1,6 @@
 class GrantsController < InheritedResources::Base
+  http_basic_authenticate_with name: ENV["HTTP_USERNAME"], password: ENV["HTTP_PASSWORD"], except: [:index, :show]
+
   def show
     @grant = Grant.find params[:id]
     @donations = Donation.successful.where("created_at >= ? AND created_at <= ?", @grant.start_date, @grant.end_date)

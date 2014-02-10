7d79fb6ff9b2692619d144bcb35a845e0038ab28
Tu Hoang <rebyn@me.com>
2014-02-10 17:25:45 +0700

    add section for finished projects on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @funding_projects = Project.funding
+    @finished_projects = Project.finished
     @markers = Project.listed.mapped
     render layout: "layouts/homepage"
   end

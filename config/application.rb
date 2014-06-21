395f329581232212764cb744d5eed1cbb68c1b09
Sean Phan <jun7thprince@gmail.com>
2014-06-21 16:32:40 +0700

    home.html.haml

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module CharityMap
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.assets.initialize_on_precompile = false
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
     # config.middleware.use Rack::SslEnforcer if Rails.env.production?
   end
 end

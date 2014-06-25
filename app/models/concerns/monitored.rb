9bc51617c6d7068baef337bd6f9b1f144b3184c7
Tu Hoang <rebyn@me.com>
2014-06-25 10:44:23 +0700

    Cleanup specs

--- app/models/concerns/monitored.rb
+++ app/models/concerns/monitored.rb
@@ -8,11 +8,13 @@ module Monitored
   end
 
   def add_new_subscriber_to_list
-    auth = {api_key: ENV['CAMPAIGN_MONITOR_API_KEY']}
-    begin
-      CreateSend::Subscriber.add auth, '2b0bc2d32f4bab37be1dcdf67fed88e6', self.email, '', [], true
-    rescue CreateSend::BadRequest => br
-      Honeybadger.notify(br)
+    if Rails.env.production?
+      auth = {api_key: ENV['CAMPAIGN_MONITOR_API_KEY']}
+      begin
+        CreateSend::Subscriber.add auth, '2b0bc2d32f4bab37be1dcdf67fed88e6', self.email, '', [], true
+      rescue CreateSend::BadRequest => br
+        Honeybadger.notify(br)
+      end
     end
   end
 

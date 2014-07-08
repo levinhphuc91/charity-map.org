1395501b17c55a4e09fa53d2752d825f8e50741c
Tu Hoang <rebyn@me.com>
2014-07-09 01:43:20 +0700

    better layout for homepage's success cases

--- config/initializers/oink.rb
+++ config/initializers/oink.rb
@@ -1,1 +1,1 @@
-CharityMap::Application.middleware.use( Oink::Middleware, :logger => Hodel3000CompliantLogger.new(STDOUT), :instruments => :memory)
+CharityMap::Application.middleware.use( Oink::Middleware, :logger => Hodel3000CompliantLogger.new(STDOUT), :instruments => :memory) if Rails.env.production?
